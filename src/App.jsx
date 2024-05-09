90d92056edf5cf39752845b32c8848eb7c8a412f
Julia Kim <juliakimseohyeon@gmail.com>
2024-05-09 09:01:44 -0400

    Resolve partials import issues in stylesheets and install Sass module

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import { useState } from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
-import "./App.css";
+import "./App.scss";
 import CollectionPage from "./pages/CollectionPage/CollectionPage";
 import FeaturedPlantsPage from "./pages/FeaturedPlantsPage/FeaturedPlantsPage";
 
