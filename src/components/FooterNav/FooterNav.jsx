549c3cba7fa6329691256046b48d5c4c49174a10
Julia Kim <juliakimseohyeon@gmail.com>
2024-05-17 10:53:20 -0400

    Link Footer component with its stylesheet

--- src/components/FooterNav/FooterNav.jsx
+++ src/components/FooterNav/FooterNav.jsx
@@ -2,30 +2,25 @@ import { Link } from "react-router-dom";
 import homeIcon from "../../assets/icons/icon-home.svg";
 import cameraIcon from "../../assets/icons/icon-camera.svg";
 import plantsIcon from "../../assets/icons/icon-plants.svg";
+import "./FooterNav.scss";
 
 export default function FooterNav() {
   return (
     <footer className="nav__bottom">
-      <ul>
-        <li>
-          <Link to="/" className="nav__bottom-item label">
-            <img className="icon" src={homeIcon} alt="Home Icon" />
-            <p>HOME</p>
-          </Link>
-        </li>
-        <li>
-          <Link to="" className="nav__bottom-item label">
-            <img className="icon" src={cameraIcon} alt="Camera Icon" />
-            <p>CAMERA</p>
-          </Link>
-        </li>
-        <li>
-          <Link to="/featuredplants" className="nav__bottom-item label">
-            <img className="icon" src={plantsIcon} alt="Plants Icon" />
-            <p>PLANTS</p>
-          </Link>
-        </li>
-      </ul>
+      <Link to="/" className="nav__bottom-item label">
+        <img className="icon" src={homeIcon} alt="Home Icon" />
+        <p>HOME</p>
+      </Link>
+
+      <Link to="" className="nav__bottom-item label">
+        <img className="icon" src={cameraIcon} alt="Camera Icon" />
+        <p>CAMERA</p>
+      </Link>
+
+      <Link to="/featuredplants" className="nav__bottom-item label">
+        <img className="icon" src={plantsIcon} alt="Plants Icon" />
+        <p>PLANTS</p>
+      </Link>
     </footer>
   );
 }
