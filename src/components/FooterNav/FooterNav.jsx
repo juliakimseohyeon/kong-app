21aa9270c9ff8d9c5d1a5fd61f5bf60af8befba2
Julia Kim <juliakimseohyeon@gmail.com>
2024-05-23 20:57:50 -0400

    Install react-webcam NPM in attempt to re-create an IOS camera screen

--- src/components/FooterNav/FooterNav.jsx
+++ src/components/FooterNav/FooterNav.jsx
@@ -6,35 +6,6 @@ import plantsIcon from "../../assets/icons/icon-plants.svg";
 import "./FooterNav.scss";
 
 export default function FooterNav() {
-  const fileInputRef = useRef(null);
-
-  const handleCameraClick = () => {
-    fileInputRef.current.click();
-  };
-
-  // const handleFileChange = async (event) => {
-  //   const file = event.target.files[0];
-  //   if (file) {
-  //     const formData = new FormData();
-  //     formData.append("image", file);
-
-  //     try {
-  //       const response = await fetch(`${import.meta.env.VITE_API_URL}/camera`, {
-  //         method: "POST",
-  //         body: formData,
-  //       });
-
-  //       if (response.ok) {
-  //         console.log("File uploaded successfully");
-  //       } else {
-  //         console.error("File upload failed");
-  //       }
-  //     } catch (error) {
-  //       console.error("Error uploading file:", error);
-  //     }
-  //   }
-  // };
-
   return (
     <footer className="nav__bottom">
       <Link to="/collections" className="nav__bottom-item label">
