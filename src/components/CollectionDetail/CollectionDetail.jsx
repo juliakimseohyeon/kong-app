2df1b3dc48a3ca5e6af15d5f8bdfaef393589060
Julia Kim <juliakimseohyeon@gmail.com>
2024-06-02 16:54:17 -0400

    Add delete and edit icons to CollectionDetail page

--- src/components/CollectionDetail/CollectionDetail.jsx
+++ src/components/CollectionDetail/CollectionDetail.jsx
@@ -1,6 +1,8 @@
 import "../../App.scss";
 import "./CollectionDetail.scss";
 import arrowLeftSmall from "../../assets/icons/icon-arrow-left.svg";
+import deleteIcon from "../../assets/icons/icon-delete.svg";
+import editIcon from "../../assets/icons/icon-edit.svg";
 import { Link } from "react-router-dom";
 
 export default function CollectionDetail({
@@ -23,36 +25,49 @@ export default function CollectionDetail({
             src={selectedPlant.image}
             alt={selectedPlant.common_name}
           />
-          <div className="hero__text">
-            <h1>{selectedPlant.common_name}</h1>
-            <h2>{selectedPlant.scientific_name}</h2>
+          <div className="hero__main-info-group">
+            <div className="hero__text">
+              <h1>{selectedPlant.common_name}</h1>
+              <h3>{selectedPlant.scientific_name}</h3>
+            </div>
+            <div className="hero__button-group">
+              <img
+                src={deleteIcon}
+                alt="Delete"
+                onClick={() => handleClickDelete(plant)}
+              />
+              <img
+                src={editIcon}
+                alt="Edit"
+                onClick={() => handleClickEdit(plant)}
+              />
+            </div>
           </div>
-          <div className="hero__status-group">
-            <p className="hero__status">{selectedPlant.status}</p>
-            <p className="hero__status">In Your Collection</p>
+          <div className="hero__status">
+            <p className="hero__status-detail">{selectedPlant.status}</p>
           </div>
         </section>
         <section className="collection-detail">
           <div className="collection-detail__block">
-            <h3>When does it grow?</h3>
+            <h2>When does it grow?</h2>
             <p className="collection-detail__description">
               {selectedPlant.season}
             </p>
           </div>
           <div className="collection-detail__block">
-            <h3>Where is it found?</h3>
+            <h2>Where is it found?</h2>
             <p className="collection-detail__description">
               {selectedPlant.habitat}
             </p>
           </div>
           <div className="collection-detail__block">
-            <h3>Characteristics</h3>
+            <h2>Characteristics</h2>
             <p className="collection-detail__description">
               {selectedPlant.characteristics}
             </p>
           </div>
           <div className="collection-detail__block">
-            <h3>Photos of the plant</h3>
+            <h2>Photos of the plant</h2>
             <img alt="" />
           </div>
         </section>
