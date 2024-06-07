1b2447c6bdb63c9787c3172a4dcf33d51ae9b141
Julia Kim <juliakimseohyeon@gmail.com>
2024-06-07 15:01:26 -0400

    Add tablet style to SearchResult

--- src/components/SearchResult/SearchResult.jsx
+++ src/components/SearchResult/SearchResult.jsx
@@ -5,14 +5,15 @@ export default function SearchResult({ plants, selectPlant, searchInput }) {
   return (
     <section className="search-result">
       <h2>Search Results</h2>
-      {plants.length > 0 ? (
-        plants.map((plant) => (
-          <Link
-            to={`/collections/${plant.id}`}
-            key={plant.id}
-            onClick={() => selectPlant(plant.id)}
-          >
-            <div className="search-result__card">
+      <div className="search-result__gallery">
+        {plants.length > 0 ? (
+          plants.map((plant) => (
+            <Link
+              to={`/collections/${plant.id}`}
+              key={plant.id}
+              onClick={() => selectPlant(plant.id)}
+              className="search-result__card"
+            >
               <img className="search-result__image" src={plant.image} />
               <div className="search-result__names">
                 <h3 className="search-result__title">{plant.common_name}</h3>
@@ -20,14 +21,14 @@ export default function SearchResult({ plants, selectPlant, searchInput }) {
                   {plant.scientific_name}
                 </p>
               </div>
-            </div>
-          </Link>
-        ))
-      ) : (
-        <p>
-          We couldn't find <b>{searchInput}</b>
-        </p>
-      )}
+            </Link>
+          ))
+        ) : (
+          <p>
+            We couldn't find <b>{searchInput}</b>
+          </p>
+        )}
+      </div>
     </section>
   );
 }
