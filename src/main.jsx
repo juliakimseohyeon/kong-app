e86526fc131c2f629e8fcbf9dedf84ea928f7288
Julia Kim <juliakimseohyeon@gmail.com>
2024-07-09 11:20:42 -0700

    Install and setup basic Auth0 settings

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,17 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { Auth0Provider } from "@auth0/auth0-react";
+import App from "./App.jsx";
+import "./index.css";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <Auth0Provider
+    domain={`${import.meta.env.VITE_AUTH_DOMAIN}`}
+    clientId={`${import.meta.env.VITE_AUTH_CLIENT_ID}`}
+    authorizationParams={{
+      redirect_uri: window.location.origin,
+    }}
+  >
     <App />
-  </React.StrictMode>,
-)
+  </Auth0Provider>
+);
